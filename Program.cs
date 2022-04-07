2a5d0282242bee5c73bf8d550a7ca4a3467f91c8
CKDenni <deni3278@easv365.dk>
2022-04-07 09:54:40 +0200

    Add CORS middleware

--- Program.cs
+++ Program.cs
@@ -2,9 +2,17 @@ using BattleshipBackend.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors();
 builder.Services.AddSignalR();
 
 var app = builder.Build();
+
+app.UseCors(policyBuilder =>
+{
+    policyBuilder.AllowAnyHeader();
+    policyBuilder.AllowAnyMethod();
+    policyBuilder.SetIsOriginAllowed(_ => true);
+});
 
 app.MapHub<ExampleHub>("/hubs/example");
 
